7c174899706d074d4ffb367e045ab8e75423367e
husneara <husnearaa99@gmail.com>
2023-09-16 11:34:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-center text-5xl'>Course Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
